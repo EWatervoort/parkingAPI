846588e3165f16146ee0549bce49c2227237f7e2
Elise Watervoort <elise@Elises-MacBook-Pro.local>
2023-03-08 11:22:52 -0700

    deleted extra comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # resources :spots, only: [:index, :show, :update, :destroy]
   get '/spots', to: 'spots#index'
   get '/spots/remaining', to: 'spots#remaining'
   get '/spots/:car_type', to: 'spots#cars_per_type'
@@ -7,9 +6,4 @@ Rails.application.routes.draw do
   patch '/spots/add/:car_type', to: 'spots#add_vehicle'
   patch '/spots/remove/:car_type', to: 'spots#remove_vehicle'
 
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
